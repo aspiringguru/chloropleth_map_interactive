c016d3c4f7ddc3e6ac9bcf97069f9fc96a3ae3c2
aspiringguru <bmatthewtaylor@gmail.com>
2020-04-17 15:40:57 +1000

    json fetch

--- world_map_data+chart/js/get_data_draw_map.js
+++ world_map_data+chart/js/get_data_draw_map.js
@@ -1,5 +1,5 @@
-console.log("screen.width:", screen.width)
-console.log("screen.height:", screen.height)
+//console.log("screen.width:", screen.width)
+//console.log("screen.height:", screen.height)
 var glob_error, glob_topo;
 
 function myFunction() {
@@ -7,37 +7,37 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "You selected: " + proj_type;
   switch (proj_type) {
     case "geoMercator":
-      console.log("geoMercator selected")
+      //console.log("geoMercator selected")
       projection = proj_geoMercator;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
       break;
     case "geoLoximuthal":
-      console.log("geoLoximuthal selected")
+      //console.log("geoLoximuthal selected")
       projection = proj_geoLoximuthal;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
       break;
     case "geoLittrow":
-      console.log("geoLittrow selected")
+      //console.log("geoLittrow selected")
       projection = proj_geoLittrow;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
       break;
     case "geoLaskowski":
-      console.log("geoLaskowski selected")
+      //console.log("geoLaskowski selected")
       projection = proj_geoLaskowski;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
       break;
     case "geoEquirectangular":
-      console.log("geoEquirectangular selected")
+      //console.log("geoEquirectangular selected")
       projection = proj_geoEquirectangular;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
       break;
     case "geoMiller":
-      console.log("geoMiller selected")
+      //console.log("geoMiller selected")
       projection = proj_geoMiller;
       svg.selectAll("*").remove();
       ready(glob_error, glob_topo)
@@ -49,10 +49,10 @@ elem_country_name = document.getElementById("country_name");
 elem_country_code = document.getElementById("country_code");
 elem_population   = document.getElementById("population");
 elem_SandP   = document.getElementById("SandP");
-console.log("elem_country_name:", elem_country_name)
-console.log("elem_country_code:", elem_country_code)
-console.log("elem_population:", elem_population)
-console.log("elem_SandP:", elem_SandP)
+//console.log("elem_country_name:", elem_country_name)
+//console.log("elem_country_code:", elem_country_code)
+//console.log("elem_population:", elem_population)
+//console.log("elem_SandP:", elem_SandP)
 // The svg
 // var element = d3.select('#myjumbo').node();
 // elem_width = element.getBoundingClientRect().width
@@ -66,9 +66,9 @@ var svg = d3.select("#my_map"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
-console.log("svg = ", svg)
-console.log("svg width = ", width)
-console.log("svg height = ", height)
+//console.log("svg = ", svg)
+//console.log("svg width = ", width)
+//console.log("svg height = ", height)
 
 // Map and projection
 var path = d3.geoPath();
@@ -120,9 +120,9 @@ var colorScale = d3.scaleThreshold()
 // Load external data and boot
 d3.queue()
   //.defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.json, "world.geojson")
+  .defer(d3.json, "data/world.geojson")
   //.defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  .defer(d3.csv, "world_pop_cr_iso.csv", function(d) {
+  .defer(d3.csv, "data/world_pop_cr_iso.csv", function(d) {
     data.set(d.code, +d.pop);
     world_pop_cr_iso.push(d)
   })
@@ -132,42 +132,42 @@ d3.queue()
   .await(ready);
 
 function get_world_pop_cr_iso(country_code){
-  console.log("country_code:",country_code)
-  console.log("world_pop_cr_iso.length:",world_pop_cr_iso.length)
+  //console.log("country_code:",country_code)
+  //console.log("world_pop_cr_iso.length:",world_pop_cr_iso.length)
   for (i = 0; i < world_pop_cr_iso.length; i++) {
     temp_ccode = world_pop_cr_iso[i]['code']
     if (temp_ccode === country_code) {
-      console.log("get_world_pop_cr_iso:found match")
+      //console.log("get_world_pop_cr_iso:found match")
       return world_pop_cr_iso[i]
     }
   }
-  console.log("get_world_pop_cr_iso:no match")
+  //console.log("get_world_pop_cr_iso:no match")
   return false
 }
 
 function ready(error, topo) {
-  console.log("error:", error)
-  console.log("topo:", topo)
+  //console.log("error:", error)
+  //console.log("topo:", topo)
   glob_error = error
   glob_topo = topo
-  console.log("world_pop_cr_iso:\n", world_pop_cr_iso)
+  //console.log("world_pop_cr_iso:\n", world_pop_cr_iso)
 
   let mouseOver = function(d) {
-    console.log("mouseOver")
-    console.log("data", data)
-    console.log("d=", d)
-    console.log("d keys = ", Object.keys(d))
-    console.log("d.id=", d.id)
-    console.log("d.properties.name=", d.properties.name)
+    //console.log("mouseOver")
+    //console.log("data", data)
+    //console.log("d=", d)
+    //console.log("d keys = ", Object.keys(d))
+    //console.log("d.id=", d.id)
+    //console.log("d.properties.name=", d.properties.name)
     d.total = data.get(d.id) || 0;
-    console.log("d.total=", d.total)
+    //console.log("d.total=", d.total)
     world_pop_cr_iso_row = get_world_pop_cr_iso(d.id)
-    console.log("get_world_pop_cr_iso output :", world_pop_cr_iso_row)
+    //console.log("get_world_pop_cr_iso output :", world_pop_cr_iso_row)
     elem_country_code.innerHTML = d.id
     elem_country_name.innerHTML = d.properties.name
     elem_population.innerHTML = "Population : "+d.total.toLocaleString()
     if (world_pop_cr_iso_row['SandP']){
-      console.log("setting elem_SandP.innerHTML, currently=",elem_SandP.innerHTML)
+      //console.log("setting elem_SandP.innerHTML, currently=",elem_SandP.innerHTML)
       elem_SandP.innerHTML = "Standard & Poors Credit Rating : "+world_pop_cr_iso_row['SandP']
     } else {
       elem_SandP.innerHTML = "Standard & Poors Credit Rating : "+"NA"
@@ -184,7 +184,7 @@ function ready(error, topo) {
   }
 
   let mouseLeave = function(d) {
-    console.log("mouseLeave")
+    //console.log("mouseLeave")
     d3.selectAll(".Country")
       .transition()
       .duration(200)
